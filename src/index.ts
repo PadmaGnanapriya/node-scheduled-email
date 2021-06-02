d18fc36f1085ceb8e6342f5ef2c96e9cc72fa384
Padma <padma@softvessel.com>
2021-06-02 15:42:51 +0530

    build error.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import {sentMail} from "./db/DbRecodeForEmail";
 const app = express();
 const PORT = "4200";
 
+
+//Hard coded exam dates. Will get from trainings
 const examTimes = [
   "Jun 01, 2021 18:00:00",
   "Jun 02, 2021 18:00:00",
@@ -13,11 +15,13 @@ const examTimes = [
 ];
 
 for (const examTime of examTimes) {
-  console.log("00000000000")
-  const timeSet: string[] = ["05:00:00", "08:00:00", "12:00:00", "16:00:00", "23:00:00"];
+  console.log("-------- test executing of this function ---------");
+  //TODO: Add the email sending time list.
+  //In here I send emails 5.00 AM, 8.00 AM, 12.00 PM, 4.00 PM, 5.00 PM, 6.00 PM, 11.00 PM
+  const timeSet: string[] = ["05:00:00", "08:00:00", "12:00:00", "16:00:00", "17:00:00", "18:00:00", "23:00:00"];
   for (const time of timeSet) {
     schedule.scheduleJob(new Date(examTime.split(" ")[1] + " " + time), () => {
-      sentMail();
+      sentMail();  // TODO: call sending email function here.
     });
   }
 }
