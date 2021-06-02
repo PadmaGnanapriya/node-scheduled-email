58c2449cc19408f5333f3090e6fba2dff7141cd1
Padma <padma@softvessel.com>
2021-06-02 15:59:20 +0530

    build error.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,32 +1,39 @@
-const webpack = require("webpack");
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const Dotenv = require('dotenv-webpack');
+
+const {
+  NODE_ENV = 'production',
+} = process.env;
 
 module.exports = {
-  entry: ["webpack/hot/poll?100", "./src/index.ts"],
-  watch: true,
-  target: "node",
-  externals: [
-    nodeExternals({
-      whitelist: ["webpack/hot/poll?100"]
-    })
-  ],
+  entry: './src/index.ts',
+  mode: NODE_ENV,
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   module: {
     rules: [
       {
-        test: /.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      },
+      {
+        type: 'javascript/auto',
+        test: /\.mjs$/,
+        use: []
       }
     ]
   },
-  mode: "development",
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-  output: {
-    path: path.join(__dirname, "build"),
-    filename: "index.js"
-  }
+  externals: [ nodeExternals() ],
+  plugins: [
+    new Dotenv()
+  ]
 };
